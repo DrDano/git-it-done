a63fa809899fd580696bbda38e79300762f1dbad
DrDano <danielharned@gmail.com>
2021-12-04 22:57:53 -0800

    adding loop to add elements to html on username submission form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTermEl = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -27,8 +29,21 @@ var getUserRepos = function(user) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTermEl.textContent = searchTerm;
     console.log(repos);
     console.log(searchTerm);
+
+    for (let i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center"
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl)
+        repoContainerEl.appendChild(repoEl);
+    }
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
