45d9462d79bc306eba3a490716b82d21914e885c
DrDano <danielharned@gmail.com>
2021-12-05 18:59:57 -0800

    adding display issues function

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,8 +4,8 @@ var getRepoIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
-            })
+                displayIssues(data);
+            });
         } else {
             alert("There was a problem with your request!");
         }
@@ -13,3 +13,26 @@ var getRepoIssues = function(repo) {
 }
 
 getRepoIssues("facebook/react");
+
+var displayIssues = function(issues) {
+    for (let i = 0; i < issues.length; i++) {
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        issueEl.appendChild(titleEl);
+
+        var typeEl = document.createElement("span");
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        issueEl.appendChild(typeEl);
+    }
+};
